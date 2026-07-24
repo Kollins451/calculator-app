8d4979bd1d252fef8d4538d6f360ed4e6c013386
Kollins451 <oladimejikollins07@gmail.com>
2026-07-24 09:58:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,30 @@
-const CACHE_NAME = "kollins-calculator-v1";
+const CACHE_NAME = "kollins-calculator-v2";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
+  "/calculator-app/",
+  "/calculator-app/index.html",
+  "/calculator-app/style.css",
+  "/calculator-app/script.js",
+  "/calculator-app/manifest.json",
+  "/calculator-app/icon.svg"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
     })
   );
 });
